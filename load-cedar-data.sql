be1ab84924328a987c222425a9a3b535cf1c871e
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2013-12-04 15:59:22 +0100

    Added SQL script skeleton for Virtuoso auto-feeding
    Former-commit-id: 0810757eec0d91a892384a21de7c01850182979b

---
+++ load-cedar-data.sql
@@ -0,0 +1,4 @@
+## /opt/virtuoso7/bin/isql 3111 dba naps48*mimed
+ld_dir('/home/virtuoso/triples2load/cedar-dump/', 'BRT_1889_02_T1_marked.ttl', 'http://lod.cedar-project.nl/resource/BRT_1889_02_T1');
+ld_dir('/home/virtuoso/triples2load/cedar-dump/', 'BRT_1889_02_T3_marked.ttl', 'http://lod.cedar-project.nl/resource/BRT_1889_02_T3');
+rdf_loader_run();
